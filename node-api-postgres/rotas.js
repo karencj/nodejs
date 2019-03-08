c38e47a28f217b7620b9b38a2d3ba68e9c35b79b
karencj <karen_catigua@hotmail.com>
2019-03-08 17:35:32 -0300

    Update rotas.js

--- node-api-postgres/rotas.js
+++ node-api-postgres/rotas.js
@@ -8,14 +8,55 @@ module.exports = {
             response.json({ info: 'Node.js, Express, and Postgres API' })
         });
         
-        app.get('/teste', async (req, res) => {
+        app.get('/testePostExterno', async (req, res) => {
 
             try {
-                const response = await fetch('https://api.github.com/users/mateusconstanzo')
-                const user = await response.json()
-                user.login = 'TESTE KAREN MUDAR LOGIN'
-                //console.log(user.login)
-                res.send(user)
+                const response = await fetch('https://httpbin.org/post', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({a: 1, b: 'Textual content'})
+                  });
+                  const content = await response.json();
+                
+                  console.log(content);
+            } catch (err) {
+                res.status(500).send({})
+            }
+        })
+
+        app.post('/testePostOutraURL', async (req, res) => {
+            console.log(req.body);
+            try {
+                const response = await fetch('https://gateway-homologation.tradeapp.com.br/auth', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        "x-api-key": "zr6Pvwuw7Q3ZVX6VQ3rDpaXdW7k768di98e5U3fI"
+                    },
+                    body: JSON.stringify({cpf: req.body.cpf , password:  req.body.password })
+                  });
+                  const content = await response.json();
+                
+                  console.log('>>>>>>>>>>>>token:' + content.data.token);
+                  //res.send(content);
+
+                  const URL =  'https://gateway-homologation.tradeapp.com.br/cliente/' + req.body.cpfCliente;
+                  const response2 = await fetch(URL, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        "x-api-key": "zr6Pvwuw7Q3ZVX6VQ3rDpaXdW7k768di98e5U3fI",
+                        "Authorization": "Bearer " + content.data.token 
+                    }//,
+                    //body: JSON.stringify({cpf: req.body.cpf , password:  req.body.password })
+                  });
+
+                  const content2 = await response2.json();
+                  console.log(content2)
+                  res.send(content2)
             } catch (err) {
                 res.status(500).send({})
             }
